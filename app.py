4dbb8cceff688fba8be46662d0f9d16e6e982f76
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2024-05-10 10:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -649,6 +649,53 @@ def recruiter_candidate_list():
         return jsonify({'error': 'Recruiter not found'})
 
 
+@app.route('/assign_candidate_new_recuriter', methods=['POST']) 
+def assign_candidate_to_a_new_recruiter():
+    data = request.json
+
+    # Check if the data is a list
+    if not isinstance(data, list):
+        return jsonify({"error": "Data must be a list of candidate objects"}), 400
+
+    try:
+        candidates_data = []
+        for candidate_data in data:
+            candidate_id = candidate_data.get('candidate_id')
+            new_recruiter_username = candidate_data.get('new_recruiter')
+            current_recruiter_username = candidate_data.get('current_recruiter')
+
+            if not candidate_id or not new_recruiter_username or not current_recruiter_username:
+                return jsonify({"error": "Candidate ID, new recruiter username, or current recruiter username not provided"}), 400
+
+            # Get the candidate, current recruiter, and the new recruiter from the database using their usernames
+            candidate = Candidate.query.filter_by(id=candidate_id, recruiter=current_recruiter_username).first()
+            current_recruiter = User.query.filter_by(username=current_recruiter_username, user_type='recruiter').first()
+            new_recruiter = User.query.filter_by(username=new_recruiter_username, user_type='recruiter').first()
+
+            if candidate is None:
+                return jsonify({"error": "Candidate not found or not assigned to current recruiter"}), 404
+
+            if current_recruiter is None:
+                return jsonify({"error": "Current recruiter not found or not a recruiter"}), 404
+
+            if new_recruiter is None:
+                return jsonify({"error": "New recruiter not found or not a recruiter"}), 404
+
+            # Update the candidate record to point to the new recruiter
+            candidate.recruiter = new_recruiter_username
+            db.session.commit()
+
+            candidates_data.append({'id': candidate.id, 'name': candidate.name})
+
+        return jsonify({
+            "message": "Candidates assigned successfully.",
+            "candidates": candidates_data
+        })
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": "Error assigning candidates: " + str(e)}), 500
+
+
 from flask import jsonify
 
 @app.route('/candidate_details/<int:candidate_id>/<user_type>/<int:page_no>', methods=['GET'])
