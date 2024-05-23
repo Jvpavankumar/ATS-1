4fc6d4c85401eba459b5c2ab9ee017fb4355ede6
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2024-05-23 11:23:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2267,9 +2267,7 @@ def view_resume(candidate_id):
 #######################################################################################
 @app.route('/upload_user_image/<int:user_id>', methods=['POST'])
 def upload_user_image(user_id):
-    data = request.json
-    print("\n\n\n\n\n")
-    print("Data :",data)
+    data = request.form
     # Extract file name and image content
     filename = data.get('file_name')
     image_content = request.files['image_file'].read()  # Retrieve binary data of the image
