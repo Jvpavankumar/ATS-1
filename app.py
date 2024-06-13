d2528c10c4b26bb9db2bb427b8f2467afe373dcb
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2024-06-13 12:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 # app.config['MAIL_USERNAME'] = 'ganesh.s@makonissoft.com'
 # app.config['MAIL_PASSWORD'] = 'Fol98135'
-app.config['MAIL_USERNAME'] = 'kanuparthisaiganesh582@gmail.com'
+app.config['MAIL_USERNAME'] = 'saiganeshkanuparthi@gmail.com'
 # app.config['MAIL_PASSWORD'] = 'Ganesh@2022'
-app.config['MAIL_PASSWORD'] = 'tozvnmxbcejynxpe'
+app.config['MAIL_PASSWORD'] = 'ungeazzlutbzkwaa'
 mail = Mail(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
